312c4863c9586eb39b48d0e0aed55b1b524b6481
Cyprien Guillemot <cyprien.guillemot@gmail.com>
2020-12-18 09:08:43 +0100

    Fix tests

--- travis_pypi_setup.py
+++ travis_pypi_setup.py
@@ -21,7 +21,7 @@ except:
     from urllib.request import urlopen
 
 
-GITHUB_REPO = 'Trax-air/swagger-parser'
+GITHUB_REPO = 'cyprieng/swagger-parser'
 TRAVIS_CONFIG_FILE = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), '.travis.yml')
 
